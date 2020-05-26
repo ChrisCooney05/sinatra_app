a37fe740cfa25eb21b1b5957960fac480037eaa7
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-26 16:27:15 +0100

    cat added to file

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'sinatra'
+
 set :session_secret, "h92hksd83ryhkwhfk39"
 
 get '/' do
   "Hello world!"
 end
 
-get '/hello/:name' do |n|
-  "Hello #{n}!"
-end
-
 get '/secret' do
   "Shush, this is a secret"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
