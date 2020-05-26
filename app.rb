0516fdc5059062a564735d15ffe9409d94d718da
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-05-26 17:10:37 +0100

    url takes name query string

--- app.rb
+++ app.rb
@@ -10,6 +10,12 @@ get '/secret' do
   "Shush, this is a secret"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
